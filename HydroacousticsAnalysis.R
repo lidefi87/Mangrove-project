a0e97d1a1be695651ea3684113fb9cd831896e05
lidefi87 <lidefi87@hotmail.com>
2018-08-22 11:57:13 +0800

    Add files via upload

--- HydroacousticsAnalysis.R
+++ HydroacousticsAnalysis.R
@@ -29,7 +29,7 @@ for(i in seq_along(BioData)){
   x = read.csv(BioData[i])
   if(i == 1){
     TotalBio = x[which(grepl("Biomass*",x$Segment.No.)&grepl("*total",x[,1])),-1]
-    MaxDepth = x[which(grepl(glob2rx("Max. range*"),x[,1])),-1]
+    MaxDepth = x[which(grepl(glob2rx("Average range*"),x[,1])),-1]
   }else{TotalBio = rbind.fill(TotalBio,x[which(grepl("Biomass*",x[,1])&grepl("*total",x[,1])),-1])
   MaxDepth = rbind.fill(MaxDepth,x[which(grepl(glob2rx("Average range*"),x[,1])),-1])}}
 rownames(TotalBio) = paste(str_sub(BioData,end = -5))
@@ -37,10 +37,11 @@ rownames(MaxDepth) = paste(str_sub(BioData,end = -5))
 for(i in seq_along(TotalBio)){
   TotalBio[,i] = as.numeric(as.character(TotalBio[,i]))
   MaxDepth[,i] = as.numeric(as.character(MaxDepth[,i]))}
-Biom3 = TotalBio/MaxDepth
-Biom3$MeanBio = rowMeans(Biom3, na.rm = T)
-TotalBio$MeanBiom3 = Biom3$MeanBio
-TotalBio$MeanDepth = rowMeans(MaxDepth, na.rm = T)
+TotalBio$TotBio = rowSums(TotalBio,na.rm = T)
+MaxDepth$MeanDepth = rowMeans(MaxDepth, na.rm = T)
+TotalBio$MeanDepth = MaxDepth$MeanDepth
+TotalBio$MeanBiogkm3 = TotalBio$TotBio/MaxDepth$MeanDepth
+TotalBio$MeanBiogkm3[30] = TotalBio$MeanBiogkm3[30]*1.6667 #Adjusting value from 600m to 1000m
 rm(MaxDepth)
 
 #Including factors for the ANOVA analysis
@@ -75,43 +76,47 @@ TotalBio = TotalBio[order(TotalBio$River,TotalBio$Distance),]
 
 #############################################Producing graphs#####################################################
 #Box plots 
-boxplot(MeanBiom3~River, data = TotalBio, ylab = "Mean Biomass (g/m3)")
+boxplot(MeanBiogkm3~River, data = TotalBio, ylab = "Mean Biomass (g/km3)")
 par(mfrow = c(2,2))
-boxplot(MeanBiom3~Distance, data = TotalBio[1:21,], ylab = "Mean Biomass (g/m3)", 
-        xlab = "Distance from river mouth (km)", ylim = c(0,4.5e-39), cex.axis = 1.5, cex.lab = 1.5)
+boxplot(MeanBiogkm3~Distance, data = TotalBio[1:21,], ylab = "Mean Biomass (g/km3)", 
+        xlab = "Distance from river mouth (km)", ylim = c(0,2.5e-38), cex.axis = 1.5, cex.lab = 1.5)
 mtext(side=3, text="Rio Doce", line = 2, cex = 1.5)
-boxplot(MeanBiom3~Distance, data = TotalBio[22:38,], ylab = "Mean Biomass (g/m3)", 
-        xlab = "Distance from river mouth (km)", ylim = c(0,4.5e-39), cex.axis = 1.5, cex.lab = 1.5)
+boxplot(MeanBiogkm3~Distance, data = TotalBio[22:38,], ylab = "Mean Biomass (g/km3)", 
+        xlab = "Distance from river mouth (km)", ylim = c(0,2.5e-38), cex.axis = 1.5, cex.lab = 1.5)
 mtext(side=3, text="Rio Jequitinhonha", line = 2, cex = 1.5)
-boxplot(MeanBiom3~Location, data = TotalBio[1:21,], ylab = "Mean Biomass (g/m3)", 
-        xlab = "Sub area (location in relation to river mouth)", ylim = c(0,4.5e-39),cex.axis = 1.5, cex.lab = 1.5)
-boxplot(MeanBiom3~Location, data = TotalBio[22:38,], ylab = "Mean Biomass (g/m3)", 
-        xlab = "Sub area (location in relation to river mouth)", ylim = c(0,4.5e-39),cex.axis = 1.5, cex.lab = 1.5)
+boxplot(MeanBiogkm3~Location, data = TotalBio[1:21,], ylab = "Mean Biomass (g/km3)", 
+        xlab = "Sub area (location in relation to river mouth)", ylim = c(0,2.5e-38),cex.axis = 1.5, cex.lab = 1.5)
+boxplot(MeanBiogkm3~Location, data = TotalBio[22:38,], ylab = "Mean Biomass (g/km3)", 
+        xlab = "Sub area (location in relation to river mouth)", ylim = c(0,2.5e-38),cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
 
 #Bar plots: mean and SE per site (one graph per river)
 par(mfrow = c(2,1))
 par(mar = c(5,6,3,2)+0.5)
-Doce = barplot(TotalBio$MeanBiom3[1:21],names.arg = TotalBio$Sitename[1:21],las=2,ylim = c(0,2.5e-38))
-mtext(side=2, text="Biomass (g/m3)", line = 5)
+Doce = barplot(TotalBio$MeanBiogkm3[1:21],names.arg = TotalBio$Sitename[1:21],las=2,ylim = c(0,4e-38))
+mtext(side=2, text="Biomass (g/km3)", line = 5)
 mtext(side=3, text="Rio Doce", line = 2)
-arrows(x0 = Doce, y0 = TotalBio$MeanBiom3[1:21], x1 = Doce, 
-       y1 = (TotalBio$MeanBiom3[1:21] + TotalBio$BiomassSE[1:21]), angle = 90, code = 3, length = 0.04, lwd = 0.4)
-Jequi = barplot(TotalBio$MeanBiom3[22:38],names.arg = TotalBio$Sitename[22:38],las=2,ylim = c(0,2.5e-38))
-mtext(side=2, text="Biomass (g/m3)", line = 5)
+arrows(x0 = Doce, y0 = TotalBio$MeanBiogkm3[1:21], x1 = Doce, 
+       y1 = (TotalBio$MeanBiogkm3[1:21] + TotalBio$BiomassSE[1:21]), angle = 90, code = 3, length = 0.04, 
+       lwd = 0.4)
+Jequi = barplot(TotalBio$MeanBiogkm3[22:38],names.arg = TotalBio$Sitename[22:38],las=2,ylim = c(0,4e-38))
+mtext(side=2, text="Biomass (g/km3)", line = 5)
 mtext(side=3, text="Rio Jequitinhonha", line = 2)
-arrows(x0 = Jequi, y0 = TotalBio$MeanBiom3[22:38], x1 = Jequi, 
-       y1 = (TotalBio$MeanBiom3[22:38] + TotalBio$BiomassSE[22:38]), angle = 90, code = 3, length = 0.04, lwd = 0.4)
+arrows(x0 = Jequi, y0 = TotalBio$MeanBiogkm3[22:38], x1 = Jequi, 
+       y1 = (TotalBio$MeanBiogkm3[22:38] + TotalBio$BiomassSE[22:38]), angle = 90, code = 3, length = 0.04,
+       lwd = 0.4)
 dev.off()
 
 #Scatterplot (Mean biomass  vs mean depth)
 par(mfrow = c(1,2))
 par(mar = c(5,5,4,2))
-plot(sqrt(sqrt(MeanBiom3[1:21])) ~ log(MeanDepth[1:21]+1), data = TotalBio, ylab = "Fourth root mean biomass (g/m3)", 
-     xlab = "Log(x+1) mean depth (m)", cex.axis = 1.25, cex.lab = 1.5, ylim = c(0, 3e-10))
+plot(sqrt(sqrt(MeanBiogkm3[1:21])) ~ log(MeanDepth[1:21]+1), data = TotalBio, 
+     ylab = "Fourth root mean biomass (g/km3)", xlab = "Log(x+1) mean depth (m)", cex.axis = 1.25, cex.lab = 1.5,
+     ylim = c(0, 4e-10))
 mtext(side=3, text="Rio Doce", line = 2, cex = 1.5)
-plot(sqrt(sqrt(MeanBiom3[22:38])) ~ log(MeanDepth[22:38]+1), data = TotalBio, ylab = "Fourth root mean biomass (g/m3)",
-     xlab = "Log(x+1) mean depth (m)", cex.axis = 1.25, cex.lab = 1.5, ylim = c(0, 3e-10))
+plot(sqrt(sqrt(MeanBiogkm3[22:38])) ~ log(MeanDepth[22:38]+1), data = TotalBio,
+     ylab = "Fourth root mean biomass (g/km3)", xlab = "Log(x+1) mean depth (m)", cex.axis = 1.25, cex.lab = 1.5,
+     ylim = c(0, 4e-10))
 mtext(side=3, text="Rio Jequitinhonha", line = 2, cex = 1.5)
 dev.off()
 
@@ -121,45 +126,49 @@ dev.off()
 
 #Biomass
 ##Log(x+1) -> Non normally distributed
-# qqnorm(log1p(TotalBio$MeanBiom3))
-# qqline(log1p(TotalBio$MeanBiom3))
-# shapiro.test(log1p(TotalBio$MeanBiom3)) #W = 0.53866, p-value = 9.366e-10
+# qqnorm(log1p(TotalBio$MeanBiogkm3))
+# qqline(log1p(TotalBio$MeanBiogkm3))
+# shapiro.test(log1p(TotalBio$MeanBiogkm3)) #W = 0.52565, p-value = 6.526e-10
 
 ###Square root(x) -> Non normally distributed
-# qqnorm(sqrt(TotalBio$MeanBiom3))
-# qqline(sqrt(TotalBio$MeanBiom3))
-# shapiro.test(sqrt(TotalBio$MeanBiom3)) #W = 0.87903, p-value = 0.0006865
+# qqnorm(sqrt(TotalBio$MeanBiogkm3))
+# qqline(sqrt(TotalBio$MeanBiogkm3))
+# shapiro.test(sqrt(TotalBio$MeanBiogkm3)) #W = 0.84305, p-value = 8.97e-05
 
 ###Fourth root(x) -> Normally distributed
-# qqnorm(sqrt(sqrt(TotalBio$MeanBiom3)))
-# qqline(sqrt(sqrt(TotalBio$MeanBiom3)))
-# shapiro.test(sqrt(sqrt(TotalBio$MeanBiom3))) #W = 0.969, p-value = 0.3655
+# qqnorm(sqrt(sqrt(TotalBio$MeanBiogkm3)))
+# qqline(sqrt(sqrt(TotalBio$MeanBiogkm3)))
+# shapiro.test(sqrt(sqrt(TotalBio$MeanBiogkm3))) #W = 0.96357, p-value = 0.2475
 
 
 #Depth
 ###Log(x+1) -> Normally distributed
 # qqnorm(log(TotalBio$MeanDepth+1))
 # qqline(log(TotalBio$MeanDepth+1))
-# shapiro.test(log(TotalBio$MeanDepth+1)) #W = 0.98081, p-value = 0.7461
+# shapiro.test(log(TotalBio$MeanDepth+1)) #W = 0.97564, p-value = 0.5644
 ###########################
 
+#Three-factor ANOVA: River, distance and location
+RivDistLoc = aov(sqrt(sqrt(MeanBiogkm3)) ~ River * Distance * Location, data = TotalBio)
+summary(RivDistLoc)
+
 #Two-factor ANOVA: River and location
-RivLoc = aov(sqrt(sqrt(MeanBiom3)) ~ River * Location, data = TotalBio)
+RivLoc = aov(sqrt(sqrt(MeanBiogkm3)) ~ River * Location, data = TotalBio)
 summary(RivLoc)
 
 #Changing Distance to factor to complete Tukey HSD post-hoc test
 TotalBio$Distance = as.factor(TotalBio$Distance)
 #Two-factor ANOVA: River and location
-RivDist = aov(sqrt(sqrt(MeanBiom3)) ~ River * Distance, data = TotalBio)
+RivDist = aov(sqrt(sqrt(MeanBiogkm3)) ~ River * Distance, data = TotalBio)
 summary(RivDist)
 #Post-hoc Tukey HSD
 PostRivDist = TukeyHSD(RivDist)
 
 #Means and SE
-RiverBio = ddply(TotalBio,.(River),summarise, meanBio = mean(MeanBiom3), seBio = std.error(MeanBiom3),
-                 minBio = min(MeanBiom3), maxBio = max(MeanBiom3))
+RiverBio = ddply(TotalBio,.(River),summarise, meanBio = mean(MeanBiogkm3), seBio = std.error(MeanBiogkm3),
+                 minBio = min(MeanBiogkm3), maxBio = max(MeanBiogkm3))
 
-DepthDif =  lm(sqrt(sqrt(MeanBiom3)) ~ MeanDepth * River, data = TotalBio)
+DepthDif =  lm(sqrt(sqrt(MeanBiogkm3)) ~ MeanDepth * River, data = TotalBio)
 summary(DepthDif)
 
 # #Pairwise t-tests - Not needed
@@ -174,25 +183,25 @@ setwd("Y:/BHP - BHP Billiton/BHP1704d Marine Survey/Data/Hydroacoustics/Data Ana
 Abiotic = read.csv("AbioticVariables.csv")
 
 #Create new dataset for linear model
-BioAbi = merge(TotalBio[,c(6:10,12)], Abiotic[,c(1:29)], by.x = "Sitename", by.y = "Site")
+BioAbi = merge(TotalBio[,c(7:11,13)], Abiotic[,c(1:29)], by.x = "Sitename", by.y = "Site")
 #Normalise abiotic data
 BioAbi.trans = scale(BioAbi[,10:ncol(BioAbi)])
 BioAbi.trans = cbind(BioAbi[1:9],BioAbi.trans)
-BioAbi.trans$FourthBio = sqrt(sqrt(BioAbi.trans$MeanBiom3))
+BioAbi.trans$FourthBio = sqrt(sqrt(BioAbi.trans$MeanBiogkm3))
 
 #Plots to check for relationship
 par(mfrow = c(3,3))
 #Doce river
 for (i in 11:ncol(BioAbi.trans)-1){
   # fit = lm(BioAbi.trans$FourthBio[1:11] ~ BioAbi.trans[1:11,i])
-  plot(BioAbi.trans$FourthBio[1:11] ~ BioAbi[1:11,i], ylab = "Fourth root mean biomass (g/m3)", 
+  plot(BioAbi.trans$FourthBio[1:11] ~ BioAbi[1:11,i], ylab = "Fourth root mean biomass (g/km3)", 
        xlab = colnames(BioAbi.trans)[i])}
   # lines(BioAbi.trans[1:11,i], fitted(fit), col="blue")}
 dev.off()
 
 par(mfrow = c(3,3))
 for (i in 10:ncol(BioAbi.trans)-1){
-  plot(BioAbi.trans$FourthBio[12:17] ~ BioAbi[12:17,i], ylab = "Fourth root mean biomass (g/m3)", 
+  plot(BioAbi.trans$FourthBio[12:17] ~ BioAbi[12:17,i], ylab = "Fourth root mean biomass (g/km3)", 
        xlab = colnames(BioAbi.trans)[i])}
 dev.off()
 
